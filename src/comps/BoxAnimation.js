cc3cf85fe3fad2131f4cde9df45e2e881bb83563
Fabian Dinkla <fabian@192.168.2.12>
2021-11-01 16:19:16 +0100

    Commit

--- src/comps/BoxAnimation.js
+++ src/comps/BoxAnimation.js
@@ -6,6 +6,7 @@ const BoxAnimation = ({ animationSRC }) => {
 				className='box-frame'
 				scrolling='no'
 				frameBorder='0'
+				loading='lazy'
 				width='900px'
 				title='boxAnimation'
 				height='641px'></iframe>
